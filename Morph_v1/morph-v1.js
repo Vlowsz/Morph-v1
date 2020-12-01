e77305953d7b85741b0e93470876fcb46766e67e
Vlowsz <m.rivals24@gmail.com>
2020-12-01 15:16:55 +0700

    modified html, js, and package

---
+++ Morph_v1/morph-v1.js
@@ -0,0 +1,63 @@
+import liff from '@line/liff';
+
+liff.init({ liffId: 'myLiffId' });
+
+window.onload = function() {
+    const useNodeJS = true;   // if you are not using a node server, set this value to false
+    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+
+    // DO NOT CHANGE THIS
+    let myLiffId = "";
+
+    if (useNodeJS) {
+        fetch('/send-id')
+            .then(function(reqResponse) {
+                return reqResponse.json();
+            })
+            .then(function(jsonResponse) {
+                myLiffId = jsonResponse.id;
+                initializeLiffOrDie(myLiffId);
+            })
+            .catch(function(error) {
+                document.getElementById("liffAppContent").classList.add('hidden');
+                document.getElementById("nodeLiffIdErrorMessage").classList.remove('hidden');
+            });
+    } else {
+        myLiffId = defaultLiffId;
+        initializeLiffOrDie(myLiffId);
+    }
+};
+
+function initializeLiffOrDie(myLiffId) {
+    if (!myLiffId) {
+        document.getElementById("liffAppContent").classList.add('hidden');
+        document.getElementById("liffIdErrorMessage").classList.remove('hidden');
+    } else {
+        initializeLiff(myLiffId);
+    }
+}
+
+function initializeLiff(myLiffId) {
+    liff
+        .init({
+            liffId: myLiffId
+        })
+        .then(() => {
+            // start to use LIFF's api
+            initializeApp();
+        })
+        .catch((err) => {
+            document.getElementById("liffAppContent").classList.add('hidden');
+            document.getElementById("liffInitErrorMessage").classList.remove('hidden');
+        });
+}
+
+function displayLiffData() {
+    document.getElementById('browserLanguage').textContent = liff.getLanguage();
+    document.getElementById('sdkVersion').textContent = liff.getVersion();
+    document.getElementById('isInClient').textContent = liff.isInClient();
+    document.getElementById('isLoggedIn').textContent = liff.isLoggedIn();
+    document.getElementById('deviceOS').textContent = liff.getOS();
+    document.getElementById('lineVersion').textContent = liff.getLineVersion();
+}
+
